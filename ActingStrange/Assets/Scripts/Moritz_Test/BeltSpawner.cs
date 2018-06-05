7e625315bec5c7d2f6fed87eef7307f442d6c88a
Moritz Naser <moritz.naser@tum.de>
2018-06-05 19:59:09 +0200

    s

---
+++ ActingStrange/Assets/Scripts/Moritz_Test/BeltSpawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltSpawner : MonoBehaviour
+{
+    [Header("Spawner Settings")]
+    public GameObject asteriod_1;
+    public GameObject asteriod_2;
+    public GameObject asteriod_3;
+    public int cubeDensity;
+    public int seed;
+    public float innerRadius;
+    public float outerRadius;
+    public float height;
+    public bool rotatingClockwise;
+
+    [Header("Asteroid Settings")]
+    public float minOrbitSpeed;
+    public float maxOrbitSpeed;
+    public float minRotationSpeed;
+    public float maxRotationSpeed;
+
+    private Vector3 localPosition;
+    private Vector3 worldOffset;
+    private Vector3 worldPosition;
+    private float randomRadius;
+    private float randomRadian;
+    private float x;
+    private float y;
+    private float z;
+
+    //================================================
+    // Random Point on a Circle given only the Angle.
+    // x = cx + r * cos(a)
+    // y = cy + r* sin(a)
+    //================================================
+    private void Start()
+    {
+        Random.InitState(seed);
+
+        for (int i = 0; i < cubeDensity; i++)
+        {
+            do
+            {
+                randomRadius = Random.Range(innerRadius, outerRadius);
+                randomRadian = Random.Range(0, (2 * Mathf.PI));
+
+                y = Random.Range(-(height /2), (height / 2));
+                x = randomRadius * Mathf.Cos(randomRadian);
+                z = randomRadius * Mathf.Sin(randomRadian);
+            }
+            while (float.IsNaN(z) && float.IsNaN(x));
+
+            localPosition = new Vector3(x, y, z);
+            worldOffset = transform.rotation * localPosition;
+            worldPosition = transform.position + worldOffset;
+            int random = Random.Range(0, 2);
+            GameObject _asteroid = null;
+            switch (random)
+            {
+                case 0:
+                    _asteroid = Instantiate(asteriod_1, worldPosition, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+                    break;
+                case 1:
+                    _asteroid = Instantiate(asteriod_2, worldPosition, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+                    break;
+                case 2:
+                     _asteroid = Instantiate(asteriod_3, worldPosition, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+                    break;
+                default:
+                    break;
+            }
+            _asteroid.AddComponent<BeltObject>().SetupBeltObject(Random.Range(minOrbitSpeed, maxOrbitSpeed), Random.Range(minRotationSpeed, maxRotationSpeed), gameObject, rotatingClockwise);
+            _asteroid.transform.SetParent(transform);
+        }
+    }
+}
