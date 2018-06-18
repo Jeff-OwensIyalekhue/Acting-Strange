1147d16e1b1cb5c1033b25309d8f020415e7fab4
Marco <marcomp96@gmail.com>
2018-06-18 13:44:28 +0200

    added basics of spell system

---
+++ ActingStrange/Assets/Scripts/SpellSystem/SpellCache.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCache : MonoBehaviour {
+
+    //members
+    private Spell[] spellCache = new Spell[4];
+
+    //base functions
+    void Start () {
+        //spellCache = spellbook.getSpellCache();
+	}
+	
+	void Update () {
+		//TODO
+	}
+
+    //methods
+}
