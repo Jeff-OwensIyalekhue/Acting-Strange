74a9794e91b20925359217f879cdaedd46839124
Marco <marcomp96@gmail.com>
2018-06-27 13:52:26 +0200

    modified spellsystem to take gameobjects

--- ActingStrange/Assets/Scripts/SpellSystem/Spellbook.cs
+++ ActingStrange/Assets/Scripts/SpellSystem/Spellbook.cs
@@ -6,68 +6,113 @@ public class Spellbook : MonoBehaviour {
 
     //members
     private int pointsToSpend;
-    private Spell[] spellList;  //list of all spells in the game
-    private Spell[] spellCache = new Spell[4]; //list of 4 cosen spells
-	//base functions
-	void Start () {
 
-	}
-	
-	void Update () {
-		
-	}
+    private GameObject selectedSpell;
+
+    private GameObject[] spellList;  //list of all spells in the game
+    private GameObject[] spellBook = new GameObject[8];   // list of spells shown in menu
+    private GameObject[] spellCache = new GameObject[4]; //list of 4 cosen spells
+
+    //base functions
+    void Start() {
+        for(int i = 0; i < spellBook.Length; i++)       //fill spellbook
+        {
+            spellBook[i] = spellList[i * 3];
+        }
+        for (int i = 0; i < spellCache.Length; i++)       //fill spellcache
+        {
+            spellCache[i] = spellBook[i * 2];
+        }
+    }
+
+    void Update() {
+
+    }
 
     //methods
-    void spendPoint(Spell spell)
+    void selectSpell(int id)
     {
-        if (spell.getCurrPoints() < spell.getPointsToUnlock() && !spell.isUnlocked())
+        selectedSpell = spellBook[id];
+        //show buttons and subtext
+    }
+    void spendPoint()
+    {
+        if (selectedSpell != null) { 
+            if (selectedSpell.GetComponent<Spell>().getCurrPoints() < selectedSpell.GetComponent<Spell>().getPointsToUnlockNext())
+            {
+                pointsToSpend--;
+                selectedSpell.GetComponent<Spell>().addCurrPoints(1);
+            }
+            else
+            {
+                Debug.Log("Point could not be spend. Already enough points.");
+            }
+        }
+        else
         {
-            pointsToSpend--;
-            spell.addCurrPoints(1);
+            Debug.Log("No spell selected.");
         }
     }
 
     void returnPoint(Spell spell)
     {
-        if (spell.getCurrPoints() > 0)
+        if (selectedSpell != null)
         {
-            spell.addCurrPoints(-1);
-            pointsToSpend++;
+            if (spell.getCurrPoints() > 0)
+            {
+                spell.addCurrPoints(-1);
+                pointsToSpend++;
+            }
+            else
+            {
+                Debug.Log("No points to return");
+            }
         }
-       
+        else
+        {
+            Debug.Log("No spell selected.");
+        }
     }
 
     void commitPoints()
     {
-        for(int i = 0; i < spellList.Length; i++)
+        for (int i = 0; i < spellBook.Length; i++)
         {
-            Spell spell = spellList[i];
-            if(spell.getCurrPoints() == spell.getPointsToUnlock())
+            GameObject spell = spellBook[i];
+            Spell spellScript = spell.GetComponent<Spell>();
+            if (spellScript.getCurrPoints() == spellScript.getPointsToUnlockNext())
             {
-                spell.unlock();
-                spell.addCurrPoints((-1)* spell.getCurrPoints());
-                Debug.Log(spell.getName() +" successfully unlocked.");
+                getNextSpell(spell).GetComponent<Spell>().unlock();
+                spellScript.addCurrPoints((-1) * spellScript.getCurrPoints());
+                spellBook[i] = getNextSpell(spell);
+                for(int j = 0; j < spellCache.Length; j++)
+                {
+                    if (spellCache[j] == spell)
+                    {
+                        spellCache[j] = getNextSpell(spell);
+                    }
+                }
+                Debug.Log(spellScript.getName() + " successfully unlocked.");
                 //TODO: popup
                 //TODO check if latest spell in cache
             }
         }
     }
 
-    void selectSpell(Spell spell)
+    public GameObject[] getSpellCache()
     {
-        if (spell.isUnlocked())
+        return spellCache;
+    }
+
+    public GameObject getNextSpell(GameObject spell)
+    {
+        if (spell.GetComponent<Spell>().getUpgradeID() != 0)
         {
-            spellCache[spell.getSubID()] = spell;
+            return spellList[spell.GetComponent<Spell>().getUpgradeID()];
         }
         else
         {
-            Debug.Log("Spell not unlocked yet.");
-            //TODO: popup
+            return null;
         }
-    }
-
-    public Spell[] getSpellCache()
-    {
-        return spellCache;
     }
 }
