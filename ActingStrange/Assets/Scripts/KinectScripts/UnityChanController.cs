bb71aeb7b8a2ca1ce9b1dd69cf6389c08e27b1ff
GameLab <thefalsekiller@gmx.de>
2018-06-27 14:12:26 +0200

    leg recognnition blocked and testing with u_character_ref

--- ActingStrange/Assets/Scripts/KinectScripts/UnityChanController.cs
+++ ActingStrange/Assets/Scripts/KinectScripts/UnityChanController.cs
@@ -6,10 +6,10 @@ public class UnityChanController : BasicAvatarController
 {	
 	public override void Start(){
         // find transforms of model
-        SpineBase = GameObject.Find("Character1_Hips").transform;
-        //SpineMid = GameObject.Find("Character1_Spine2").transform;
-        Neck = GameObject.Find("Character1_Neck").transform;
-        Head = GameObject.Find("Character1_Head").transform;
+        //SpineBase = GameObject.Find("Character1_Hips").transform;//
+        SpineMid = GameObject.Find("Character1_Spine2").transform;
+        Neck = GameObject.Find("Character1_Neck").transform;//
+        Head = GameObject.Find("Character1_Head").transform;//
         ShoulderLeft = GameObject.Find("Character1_LeftArm").transform;
         ElbowLeft = GameObject.Find("Character1_LeftForeArm").transform;
         WristLeft = GameObject.Find("Character1_LeftHand").transform;
@@ -18,20 +18,20 @@ public class UnityChanController : BasicAvatarController
         ElbowRight = GameObject.Find("Character1_RightForeArm").transform;
         WristRight = GameObject.Find("Character1_RightHand").transform;
         //HandRight = GameObject.Find("").transform;
-        HipLeft = GameObject.Find("Character1_LeftUpLeg").transform;
-        KneeLeft = GameObject.Find("Character1_LeftLeg").transform;
-        AnkleLeft = GameObject.Find("Character1_LeftFoot").transform;
-        FootLeft = GameObject.Find("Character1_LeftToeBase").transform;
-        HipRight = GameObject.Find("Character1_RightUpLeg").transform;
-        KneeRight = GameObject.Find("Character1_RightLeg").transform;
-        AnkleRight = GameObject.Find("Character1_RightFoot").transform;
-        FootRight = GameObject.Find("Character1_RightToeBase").transform;
-        SpineShoulder = GameObject.Find("Character1_Spine2").transform;
+        //HipLeft = GameObject.Find("Character1_LeftUpLeg").transform;//
+        //KneeLeft = GameObject.Find("Character1_LeftLeg").transform;//
+        //AnkleLeft = GameObject.Find("Character1_LeftFoot").transform;//
+        //FootLeft = GameObject.Find("Character1_LeftToeBase").transform;//
+        //HipRight = GameObject.Find("Character1_RightUpLeg").transform;//
+        //KneeRight = GameObject.Find("Character1_RightLeg").transform;//
+        //AnkleRight = GameObject.Find("Character1_RightFoot").transform;//
+        //FootRight = GameObject.Find("Character1_RightToeBase").transform;//
+        SpineShoulder = GameObject.Find("Character1_Spine2").transform;//
         //HandTipLeft = GameObject.Find("Character1_LeftHandIndex1").transform;
         //ThumbLeft = GameObject.Find("Character1_LeftHandThumb1").transform;
         //HandTipRight = GameObject.Find("Character1_RightHandIndex1").transform;
         //ThumbRight = GameObject.Find("Character1_RightHandThumb1").transform;
-        
+
         base.Start();
     }
 
