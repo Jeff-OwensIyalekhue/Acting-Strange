2ea0273101b9150890df45fb042e65a539252bcb
pompeiic <naser.moritz.bsc@gmx.de>
2018-06-29 13:32:11 +0200

    spell scripts
    projectile scripts

---
+++ ActingStrange/Assets/Scripts/SpellSystem/SimpleProjectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleProjectile : Spell {
+
+    public Transform target;
+    public float speed=0.1f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
+        //vllt mit look at besser..
+        transform.LookAt(target);
+    }
+}
