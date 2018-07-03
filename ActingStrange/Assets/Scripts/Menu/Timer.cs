4aa5d12e2f9eae3fa4e7223a91ab2442906bd29c
Jeff-Owens Iyalekhue <j.oiyalekhue@hotmail.de>
2018-07-03 11:25:11 +0200

    addition to last commit
    und nochmal aber dieses mal bitte mit allem was ich ausgewählt habe... git

---
+++ ActingStrange/Assets/Scripts/Menu/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text text;
+    private float t;
+
+	// Use this for initialization
+	void Start () {
+        t = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "" + (int)(Time.time - t) + " sec.";
+	}
+}
