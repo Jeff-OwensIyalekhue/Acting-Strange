afd46205455348539b8169f9d1f72a9c4d3b7d54
Moritz Naser <moritz.naser@tum.de>
2018-07-04 23:47:23 +0200

    enemy script fertig

--- ActingStrange/Assets/Scripts/WaveManager/Enemy.cs
+++ ActingStrange/Assets/Scripts/WaveManager/Enemy.cs
@@ -1,26 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 
     //members
     private string name;
     private int ID;
-
+    private NavMeshAgent agent;
+    private Animator animator;
     //enemy attributes
     private float health;
     private float speed;
     private float slow;
     private float dot;
-
+    private bool start = false;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //TODO: movement
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        animator = gameObject.GetComponent<Animator>();
+        Debug.Log("start couritine");
+        StartCoroutine(TestCoroutine());
+
+    }
+
+    IEnumerator TestCoroutine()
+    {
+        yield return new WaitForSeconds(3.5f);
+        start = true;
+        Debug.Log("back");
+    }
+
+        // Update is called once per frame
+        void Update () {
+
+        if (start)
+        {
+            GameObject[] targets = GameObject.FindGameObjectsWithTag("Player");
+            GameObject target = null;
+
+            //select nearest
+            float maxdistance = 10000000f;
+
+            //findet nächsten enemy und setzt ihn als target
+            foreach (GameObject t in targets)
+            {
+                float distance = Vector3.Distance(t.transform.position, transform.position);
+
+                if (distance <= maxdistance)
+                {
+                    target = t;
+                    maxdistance = distance;
+                    if (distance <= 2.5)
+                    {
+                        animator.SetBool("Attack", true);
+                    }
+                    else
+                    {
+
+                        animator.SetBool("Attack", false);
+                    }
+                }
+            }
+            //TODO: movement
+            agent.SetDestination(target.transform.position);
+        }
     }
 }
