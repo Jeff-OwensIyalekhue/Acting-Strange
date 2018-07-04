7c67c2fcea9358c2d73fb3b312ad79bdfa441276
GameLab <thefalsekiller@gmx.de>
2018-07-04 11:52:24 +0200

    strngecontroller added

--- ActingStrange/Assets/Scripts/KinectScripts/UnityChanController.cs
+++ ActingStrange/Assets/Scripts/KinectScripts/UnityChanController.cs
@@ -7,16 +7,16 @@ public class UnityChanController : BasicAvatarController
 	public override void Start(){
         // find transforms of model
         //SpineBase = GameObject.Find("Character1_Hips").transform;//
-        SpineMid = GameObject.Find("Character1_Spine2").transform;
+        SpineMid = GameObject.Find("Character1_Spine2").transform;//
         Neck = GameObject.Find("Character1_Neck").transform;//
         Head = GameObject.Find("Character1_Head").transform;//
-        ShoulderLeft = GameObject.Find("Character1_LeftArm").transform;
-        ElbowLeft = GameObject.Find("Character1_LeftForeArm").transform;
-        WristLeft = GameObject.Find("Character1_LeftHand").transform;
+        ShoulderLeft = GameObject.Find("Character1_LeftArm").transform;//
+        ElbowLeft = GameObject.Find("Character1_LeftForeArm").transform;//
+        WristLeft = GameObject.Find("Character1_LeftHand").transform;//
         //HandLeft = GameObject.Find("").transform;
-        ShoulderRight = GameObject.Find("Character1_RightArm").transform;
-        ElbowRight = GameObject.Find("Character1_RightForeArm").transform;
-        WristRight = GameObject.Find("Character1_RightHand").transform;
+        ShoulderRight = GameObject.Find("Character1_RightArm").transform;//
+        ElbowRight = GameObject.Find("Character1_RightForeArm").transform;//
+        WristRight = GameObject.Find("Character1_RightHand").transform;//
         //HandRight = GameObject.Find("").transform;
         //HipLeft = GameObject.Find("Character1_LeftUpLeg").transform;//
         //KneeLeft = GameObject.Find("Character1_LeftLeg").transform;//
